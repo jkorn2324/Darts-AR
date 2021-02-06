4f8d55fff40722ba722f998ee8f79ae0320777e9
jkorn2324 <jkorn2324swagg@gmail.com>
2021-02-06 01:45:37 -0800

    Started implementing game players

---
+++ Assets/Scripts/Game/Player/GamePlayer.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ModifiedObject.Scripts.Game.Player
+{
+
+    /// <summary>
+    /// The player color.
+    /// </summary>
+    public enum PlayerColor
+    {
+        COLOR_RED,
+        COLOR_BLUE,
+        COLOR_GREEN,
+        COLOR_BLACK,
+        COLOR_YELLOW,
+        COLOR_WHITE
+    }
+
+    public class PlayerScore
+    {
+        #region fields
+
+        private Utils.References.IntegerReference _currentPlayerScore;
+        private int _score;
+        private readonly GamePlayer _parent;
+
+        #endregion
+
+        #region properties
+
+        public int Score
+        {
+            get => this._score;
+            set => this._score = value;
+        }
+
+        #endregion
+
+        #region constructor
+
+        public PlayerScore
+            (GamePlayer player, Utils.References.IntegerReference currentPlayerScore)
+        {
+            this._score = 0;
+            this._currentPlayerScore = currentPlayerScore;
+            this._parent = player;
+        }
+
+        #endregion
+
+        #region methods
+
+        /// <summary>
+        /// Called when the current player has been switched as active.
+        /// </summary>
+        public void OnSetCurrentThrowingPlayer()
+        {
+            this._currentPlayerScore.Value = this._score;
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// The Game player class.
+    /// </summary>
+    public class GamePlayer
+    {
+
+        #region fields
+
+        private readonly string _name;
+        private readonly PlayerColor _color;
+        private PlayerScore _score;
+
+        #endregion
+
+        #region properties
+
+        public string Name
+            => this._name.ToLower();
+
+        public PlayerColor PlayerColor
+            => this._color;
+
+        public PlayerScore Score
+            => this._score;
+
+        #endregion
+
+        #region constructor
+
+        public GamePlayer(string name, PlayerColor color,
+            Utils.References.IntegerReference currentPlayerScore)
+        {
+            this._name = name;
+            this._color = color;
+            this._score = new PlayerScore(this, currentPlayerScore);
+        }
+
+        #endregion
+
+        #region methods
+
+        /// <summary>
+        /// Called when this player has become the current player
+        /// to throw.
+        /// </summary>
+        public void SetCurrentThrowingPlayer()
+        {
+            this._score.OnSetCurrentThrowingPlayer();
+        }
+
+        #endregion
+    }
+}
+
---
+++ Assets/Scripts/Game/Player/GamePlayerSet.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace ModifiedObject.Scripts.Game.Player
+{
+
+    /// <summary>
+    /// The result of when the player gets added.
+    /// </summary>
+    public enum AddPlayerResult
+    {
+        RESULT_FAILED_NAME,
+        RESULT_FAILED_COLOR,
+        RESULT_FAILED_MAXPLAYERS,
+        RESULT_SUCCESS
+    }
+
+    /// <summary>
+    /// The set of players.
+    /// </summary>
+    [CreateAssetMenu(fileName = "Game Player Set", menuName = "Game/Game Player Set")]
+    public class GamePlayerSet : ScriptableObject
+    {
+
+        #region fields
+
+        [SerializeField]
+        private Utils.References.IntegerReference maxPlayers;
+        [SerializeField]
+        private Utils.Events.GameEvent activePlayerChanged;
+
+        private int _minPlayers = 1;
+        private List<GamePlayer> _players
+            = new List<GamePlayer>();
+        private int _currentActiveIndex = 0;
+
+        #endregion
+
+        #region properties
+
+        public int MinPlayers
+            => this._minPlayers;
+
+        public int CurrentPlayers
+            => this._players.Count;
+
+        public int MaxPlayers
+            => this.maxPlayers.Value;
+
+        public GamePlayer ActivePlayer
+            => this._players[this._currentActiveIndex];
+
+        #endregion
+
+        #region methods
+
+        public AddPlayerResult AddGamePlayer(GamePlayer player)
+        {
+            if(this.CurrentPlayers >= this.MaxPlayers)
+            {
+                return AddPlayerResult.RESULT_FAILED_MAXPLAYERS;
+            }
+
+            GamePlayer byName = this.GetGamePlayer(player.Name);
+            if(byName != null)
+            {
+                return AddPlayerResult.RESULT_FAILED_NAME;
+            }
+
+            GamePlayer byColor = this.GetGamePlayerFromColor(player.PlayerColor);
+            if(byColor != null)
+            {
+                return AddPlayerResult.RESULT_FAILED_COLOR;
+            }
+
+            this._players.Add(player);
+            return AddPlayerResult.RESULT_SUCCESS;
+        }
+
+        public GamePlayer GetGamePlayer(string name)
+        {
+            return this._players.Find((GamePlayer searched) =>
+            {
+                return searched.Name == name;
+            });
+        }
+
+        public GamePlayer GetGamePlayerFromColor(PlayerColor color)
+        {
+            return this._players.Find((GamePlayer searched) =>
+            {
+                return searched.PlayerColor == color;
+            });
+        }
+
+        public void SetNextPlayerAsActive()
+        {
+            if(++this._currentActiveIndex > this.CurrentPlayers)
+            {
+                this._currentActiveIndex = 0;
+            }
+            this.activePlayerChanged?.CallEvent();
+        }
+
+        #endregion
+    }
+}
+
